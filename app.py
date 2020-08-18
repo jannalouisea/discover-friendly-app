437deeacd299d128faf8852f8db4dfac0a63c00b
Janna Agustin <jannalouisea@gmail.com>
2020-08-18 00:47:47 -0400

    fixed redirect uri again

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.secret_key = "WX`8$8E21'$2N|{\x16"
 app.static_folder = 'static'
 
 API_BASE = 'https://accounts.spotify.com'
-REDIRECT_URI = "http://127.0.0.1:5000/api_callback"
+REDIRECT_URI = "https://discover-friendly.herokuapp.com/api_callback"
 SCOPE = 'playlist-modify-private,playlist-modify-public,user-top-read, user-library-read'
 SHOW_DIALOG = True # False later
 
